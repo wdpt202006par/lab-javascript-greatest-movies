d5d7cb4f1dcf0443fca84a9bb0e851c93c3b0bd0
toufiq23 <drrameraaron023@gmail.com>
2020-06-12 17:52:22 +0200

    Lab Practice

--- src/movies.js
+++ src/movies.js
@@ -1,92 +1,79 @@
 // Iteration 1: All directors? - Get the array of all directors.
 
 function getAllDirectors(movies) {
-	const directors = movies.map(key => key.director);
+	const directors = 
 	return directors;
 }
 getAllDirectors(movies);
 
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
-function removeDuplicates(originalArray, prop) {
-	let newArray = [];
-	let lookupObject  = {};
+// function removeDuplicates(originalArray, prop) {
+// 	let newArray = [];
+// 	let lookupObject  = {};
 
-	for(let i in originalArray) {
-		 lookupObject[originalArray[i][prop]] = originalArray[i];
-	}
+// 	for(let i in originalArray) {
+// 		 lookupObject[originalArray[i][prop]] = originalArray[i];
+// 	}
 
-	for(i in lookupObject) {
-			newArray.push(lookupObject[i]);
-	}
-	 return newArray;
-}
+// 	for(i in lookupObject) {
+// 			newArray.push(lookupObject[i]);
+// 	}
+// 	 return newArray;
+// }
 
-let uniqueArray = removeDuplicates(movies, "director");
-console.log("uniqueArray is: " + JSON.stringify(uniqueArray));
+// let uniqueArray = removeDuplicates(movies, "director");
+// console.log("uniqueArray is: " + JSON.stringify(uniqueArray));
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
-function howManyMovies(movies) {
-	const stevenDrama = movies.filter(key => key.director === "Steven Spielberg").filter(key => key.genre.includes("Drama")).length;
-	return stevenDrama;
-};
+// function howManyMovies(movies) {
+// 	const stevenDrama = 
+// 	return stevenDrama;
+// };
 
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
 
-function ratesAverage(movies) {
-	const totalMovies = movies.length;
-	let sumAverage = movies.reduce((acc, key) => {
-		if (key.rate) {
-			return acc + key.rate
-		} else {
-			return acc
-		}
-	}, 0)
-		if (movies.length === 0) {
-			return 0;
-		}
-			return Math.round((sumAverage / totalMovies) * 100) / 100;
-}
-ratesAverage(movies);
+// function ratesAverage(movies) {
+// 	const totalMovies = movies.length;
+// 	let sumAverage = movies.reduce((acc, key) => {
+		
+// 	}, 0)
+		
+// }
+// ratesAverage(movies);
 
 // Iteration 4: Drama movies - Get the average of Drama er(key) Movies
 
-function dramaMoviesRate(movies) {
-	const dramaMovies = movies.filter(key => key.genre.includes("Drama")).filter(key => key.rate)
-	if (dramaMovies.length === 0) {
-		return 0;
-	}
-	const averageRateDrama = dramaMovies.reduce((acc, key) => acc + key.rate, 0) / dramaMovies.length;
-	return Math.round(averageRateDrama * 100) / 100;
-}
-dramaMoviesRate(movies);
+// function dramaMoviesRate(movies) {
+// 	const dramaMovies = 
+
+// 	const averageRateDrama = 
+// }
+// dramaMoviesRate(movies);
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 
-function orderByYear(movies) {
-	let newArray = [...movies];
-	newArray.sort(function(a, b) {
-		if (a.year === b.year) {
-			return (a.title).localeCompare(b.title)
-		}
-		return (a.year - b.year)
-	})
-	return newArray;
-}
-orderByYear(movies);
+// function orderByYear(movies) {
+// 	let newArray = [...movies];
+// 	newArray.sort(function(a, b) {
+		
+// 	})
+// 	return newArray;
+// }
+// orderByYear(movies);
 
 
 // Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
 
-function orderAlphabetically(movies) {
-	let titleArray = [...movies];
-	const orderedTitle = titleArray.sort((a, b) => a.title.localeCompare(b.title))
+// function orderAlphabetically(movies) {
+// 	let titleArray = [...movies];
+// 	const orderedTitle = 
 
-	const twenty = orderedTitle.map(key => key.title).slice(0, 20);
-	return twenty;
-}
-orderAlphabetically(movies);
+// 	const twenty = 
+// 	return twenty;
+// }
+// orderAlphabetically(movies);
 
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
